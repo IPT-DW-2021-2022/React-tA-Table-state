0ff0b65c4cb8ddb18144e27e8076dae0874fad08
jcnpereira <casimiro@ipt.pt>
2022-05-31 15:35:50 +0100

    show table data, as the props example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * App.js
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import Table from "./Table03";
+
+class App extends React.Component {
+  // data is defined as 'state'
+  // this means that data can be changed by app,
+  // at real time 
+  state = {
+    students: [
+      {
+        name: "Ana",
+        surname: "Lopes",
+      },
+      {
+        name: "Mário",
+        surname: "Silva",
+      },
+      {
+        name: "Maria",
+        surname: "Pereira",
+      },
+      {
+        name: "Luís",
+        surname: "Gomes",
+      },
+    ]
+  }
+
+  render() {
+    const { students } = this.state // <=> const {students}=this.state.students
+
+    return (
+      <div className="container">
+        <h1>Students</h1>
+        <Table studentsDataIN={students} />
+      </div>
+    )
+  }
 }
 
 export default App;
